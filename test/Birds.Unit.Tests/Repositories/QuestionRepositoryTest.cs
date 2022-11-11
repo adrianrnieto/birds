0943da5a894e36669234f3bd7e4be9fd0c2ee931
martamrp <martaromeroperez86@gmail.com>
2022-11-11 22:49:17 +0100

    BIR-13: add test for repository

---
+++ test/Birds.Unit.Tests/Repositories/QuestionRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Birds.Server.Repositories;
+
+namespace Birds.Unit.Tests.Repositories
+{
+    public class QuestionRepositoryTest
+    {
+        private QuestionRepository _repository;
+
+        public QuestionRepositoryTest()
+        {
+            _repository= new QuestionRepository();
+        }
+
+        [Fact]
+        public void GetRandom_ReturnsRandomQuestion()
+        {
+            //Arrange
+
+            //Act
+            var actual = _repository.GetRandom();
+
+            //Assert
+            actual.ImageUrl.Should().NotBeEmpty();
+            actual.Answers.Should().HaveCount(4);
+            actual.CorrectAnswer.Should().NotBeEmpty();
+        }
+    }
+}
