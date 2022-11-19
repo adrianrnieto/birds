51be9d4537e3eb9d8b7ce2552f2edb6058180e64
Adrián Rodríguez Nieto <adrianrodrigueznieto89@gmail.com>
2022-11-19 21:39:54 +0100

    BIR-3: Create db migrations project

---
+++ src/Tools/Birds.Db.Migrations/Program.cs
@@ -0,0 +1,45 @@
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Birds.Db.Migrations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build()
+                .GetConnectionString("Birds");
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return;
+        }
+    }
+}
